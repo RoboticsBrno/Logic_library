d8b3d7a72ffa0310c070bf87c16c690c1233d6af
Vojtěch Boček <vojtech.bocek@avast.com>
2021-07-15 15:24:52 +0200

    Add github actions to build examples

---
+++ build_examples.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+failed=false;
+
+base="$(pwd)"
+for d in $(find examples -maxdepth 1 -mindepth 1 -type d); do
+    echo "::group::$(basename $d)"
+    cd "$d"
+    if ! platformio run -e normal; then
+        echo "::error ::Failed to build $d"
+        failed=true;
+    fi
+    cd "$base"
+    echo "::endgroup::"
+done
